fd80e2c0fa487277d27ae037de42a91e4d208899
Gabriel Vieira <gabrielvieira1@protonmail.com>
2024-01-14 21:03:39 -0300

    Project created

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      this.InitializeComponent();
+
+      var BI = new BitmapImage(new Uri("ms-appx:///eclipse.gif"));
+
+      LIB14393.AnimatedGif.SetAutoPlayOff(BI);
+
+      // Sem suporte para o build 14393
+      // BI.AutoPlay = false;
+
+      var Image = new Image();
+      Image.Source = BI;
+      this.Content = Image;
+    }
+  }
+}
---
+++ LIB14393/AnimatedGif.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace LIB14393
+{
+  public class AnimatedGif
+  {
+    public static void SetAutoPlayOff(BitmapImage BI)
+    {
+      if (ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 3))
+      {
+        BI.AutoPlay = false;
+      }
+    }
+  }
+}
